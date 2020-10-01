caea5e2a82fd87cf5720c25f7b2477da3532baac
Carla <72182774+Carlanbv@users.noreply.github.com>
2020-10-01 01:50:57 -0400

    Add files via upload

---
+++ dom.js
@@ -0,0 +1,69 @@
+{
+var bio = document.getElementById("bio");
+    boton = document.getElementById("boton");
+    
+function cambiar ()
+    {
+        bio.style.background = "#8395a7";
+        bio.style.color = "white";
+    }
+boton.onclick=cambiar;
+}    
+
+{
+var experiencia = document.getElementById("experiencia");
+    exp1 = document.getElementById("exp1");
+    exp2 = document.getElementById("exp2");
+    desExp1 = document.getElementById("desExp1");
+    desExp2= document.getElementById("desExp2");
+    
+    boton1 = document.getElementById("boton1");
+
+function cambiar ()
+{
+    experiencia.style.background = "#8395a7";
+    exp1.style.color= "white";
+    exp2.style.color="white";
+    desExp1.style.color="white";
+    desExp2.style.color="white";
+}
+
+boton1.onclick=cambiar;
+}
+
+{
+    var edu = document.getElementById("edu");
+        descrCole=document.getElementById("descrCole");
+        nombreCole=document.getElementById("nombreCole");
+        lugarIdiom=document.getElementById("lugarIdiom");
+        descIdiom=document.getElementById("descIdiom");
+        uni=document.getElementById("uni");
+        descUni=document.getElementById("descUni");
+
+         boton2 = document.getElementById("boton2");
+    
+    function cambiar ()
+    {
+        edu.style.background = "#8395a7";
+        descrCole.style.color="white";
+        nombreCole.style.color="white";
+        lugarIdiom.style.color="white";
+        descIdiom.style.color="white";
+        uni.style.color="white";
+        descUni.style.color="white";
+    }
+    
+    boton2.onclick=cambiar;
+}
+
+{
+    var idiom = document.getElementById("idiom");
+         boton3 = document.getElementById("boton3");
+    
+    function cambiar ()
+    {
+        idiom.style.background = "#8395a7";
+    }
+    
+    boton3.onclick=cambiar;
+    }
